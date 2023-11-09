12b87e7ea013c46049af6a3d7f1b0d31fa849684
Jaider Barrera <barrerajaider22@gmail.com>
2023-11-08 20:40:37 -0500

    Elimine algunos comentarios debido a que me causaban confusion en la hora de analziar la sintaxis

--- parte1/conexion.php
+++ parte1/conexion.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = "localhost"; // Tu servidor de base de datos
-$db_nombre = "usuario"; // Nombre de tu base de datos
-$db_usuario = "root"; // Nombre de usuario de la base de datos
-$db_contra = ""; // Contraseña de tu base de datos
+$db_host = "localhost"; 
+$db_nombre = "usuario"; 
+$db_usuario = "root"; 
+$db_contra = ""; 
 
 $conexion = mysqli_connect($db_host, $db_usuario, $db_contra, $db_nombre);
 
